eb4352811dff42973ebc03076ab91111a2583647
kirankaranth1 <kirankaranth1@gmail.com>
2015-01-15 16:06:42 +0530

    Added comments on checkifhome.py

--- checkifhome.py
+++ checkifhome.py
@@ -13,14 +13,15 @@ def checkifhome(teamname,fixtures):
             return False
         else:
             continue
-            
-            
+          
+##This part is just to test the module. Not required at all            
 if __name__ == "__main__":
     gw=sys.argv[1]
     print(gw)
     res = requests.get("http://www.football-data.org/soccerseasons/354/fixtures?matchday="+str(gw))
     fixtures=json.loads(res.text)
+    
     if checkifhome('Liverpool'):
         print 'home'
     else:
-        print 'false'
+        print "false"
